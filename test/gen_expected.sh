e9e60a059333cfafb9b1c960f83d9d40cba6313d
Andrew Seward <awswrd@gmail.com>
2021-12-04 15:43:30 -0800

    Add some very basic coverage for `_list_files`
    Left to do:
    - Probably some rework
    - Fit it into CI process

---
+++ test/gen_expected.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Follows a pattern from here that I really liked:
+# https://web.archive.org/web/20211130220046/https://www.oilshell.org/blog/2021/08/xargs.html
+#
+# (look for `xargs Can Invoke Shell Functions With the $0 Dispatch Pattern`)
+
+one() {
+  echo "./files/$1"
+}
+
+many() {
+  gxargs -n 1 -d $'\n' -- "$0" one | sort -u
+}
+
+"$@"
